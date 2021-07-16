015ddc6d3289c2f4c035df67cabcd64ee26b2cdb
Brandon De Leon <brandondeleon.615@gmail.com>
2021-07-16 11:54:05 -0500

    inital commit

---
+++ shopping.js
@@ -0,0 +1,29 @@
+// Open shopping.js and declare a variable whose value will 
+// be an array filled with objects. Each object will represent an
+//  item that you first think of when you go to the store.
+// 
+const groceryList =[
+   
+]
+
+const eggs ={
+    id:1,
+    name:'eggs',
+    price: 5
+}
+
+const milk = {
+    id: 2,
+    name:'milk',
+    price: 4
+}
+
+const mustard = {
+    id: 3,
+    name:'mustard',
+    price: 6
+}
+
+
+groceryList.push(eggs,milk,mustard);
+console.log(groceryList);
